94cac7c8e661a17f693a838b7114145b8ca1f1e7
nashirjamali <nashirjamali@gmail.com>
2019-12-07 16:19:13 +0700

    first commit

---
+++ resources/views/include/script-header.blade.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="{{ asset('concept/vendor/bootstrap/css/bootstrap.min.css') }}">
+<link href="{{ asset('concept/vendor/fonts/circular-std/style.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('concept/libs/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('concept/vendor/fonts/fontawesome/css/fontawesome-all.css') }}">
+<link rel="stylesheet" href="{{ asset('concept/vendor/charts/chartist-bundle/chartist.css') }}">
+<link rel="stylesheet" href="{{ asset('concept/vendor/charts/morris-bundle/morris.css') }}">
+<link rel="stylesheet" href="{{ asset('concept/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('concept/vendor/charts/c3charts/c3.css') }}">
+<link rel="stylesheet" href="assets/vendor/fonts/flag-icon-css/flag-icon.min.css') }}">
